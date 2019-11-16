123dc614ad8ad5217ed83dd1cff9eefd941e8421
joserestrepo <joserestrepo301@gmail.com>
2019-11-16 11:12:41 -0500

    mejora en el servidor

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
 
 const app = express();
 
 app.use(express.static(__dirname+'dist'));
-app.get('/',function(req,res){
+app.get('*',function(req,res){
     res.sendFile(path.join(__dirname+'dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3001);
+app.set('port',port);
+
+const server = http.createServer(app)
+server.listen(port, ()=> console.log('Running'));
